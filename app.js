150cbd64414ca725fa4df7b41424a78ba0f5b329
Brittany Javalera <brittjavalera@gmail.com>
2020-12-05 19:55:50 -0800

    added new variables for different data and are sending them to the browser via res.write and res.send

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.get("/", function(req, res){
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
             const description = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<h1>The temperature in Los Angeles is " + temp + " degrees F </h1>")
+            res.write("<p>The weather is currently " + description + "</p>")
+            res.write("<img src=" + imageURL + ">")
+            res.send()
         })
     })
 })
